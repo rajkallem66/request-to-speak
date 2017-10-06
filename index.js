0d31ee105c266c4f0234619432fc57bc4e3d98b9
Guy Sperry <sperrygu@saccounty.net>
2017-10-06 14:01:24 -0700

    Added syslog logging functionality.

--- index.js
+++ index.js
@@ -2,16 +2,32 @@
 "use strict";
 let winston = require("winston");
 let config = require("config");
-winston.setLevels(config.get("RTS.log.levels"));
+let logTransports = config.get("RTS.log.transports");
+let logOptions = {
+    transports: [],
+    levels: config.get("RTS.log.levels"),
+    level: config.get("RTS.log.level")
+};
+logTransports.forEach(function(t) {
+    switch(t.name) {
+    case "console": 
+        logOptions.transports.push(new winston.transports.Console(t.options));
+        break;
+    case "syslog":
+        require('winston-syslog').Syslog;
+        let sysTransport = new winston.transports.Syslog(t.options)
+        logOptions.transports.push(sysTransport);
+        break;
+    }
+});
+
+let logger = new winston.Logger(logOptions);
 winston.addColors(config.get("RTS.log.colors"));
-winston.level = config.get("RTS.log.level");
-winston.remove(winston.transports.Console);
-winston.add(winston.transports.Console, {colorize: true});
 
 // Sometimes things to awry.
 process.on("uncaughtException", function(err) {
-    winston.emergency("uncaughtException:", err.message);
-    winston.emergency(err.stack);
+    logger.emerg("uncaughtException:", err.message);
+    logger.emerg(err.stack);
     process.exit(1);
 });
 
@@ -28,12 +44,12 @@ let port;
 try {
     port = config.get("RTS.port");
 } catch(e) {
-    winston.info("No port specified.");
+    logger.info("No port specified.");
 }
 
 app.set("port", port || 3000);
 app.use(favicon(__dirname + "/client/favicon.ico"));
-app.use(bodyParser.json());
+app.use(bodyParser.json());-
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, "client")));
 
@@ -42,43 +58,43 @@ let transformer = config.get("RTS.wsTransformer");
 let primus = new Primus(server, {transformer: transformer});
 
 server.listen(app.get("port"), function() {
-    winston.info("Express server listening on port " + app.get("port"));
+    logger.info("Express server listening on port " + app.get("port"));
 });
 
-let rtsWsApi = require("./app/rts-ws-api")(primus, winston);
-winston.info("RTS WS API Version: " + rtsWsApi.version);
+let rtsWsApi = require("./app/rts-ws-api")(primus, logger);
+logger.info("RTS WS API Version: " + rtsWsApi.version);
 
 let dbApi = config.get("RTS.dbApi");
 let dbConfig = config.get("RTS.dbConfig");
 
 // You can create your own API for Cassandra, Mongo, Oracle, etc. Just adhere to the interface.
-let rtsDbApi = require(dbApi)(dbConfig, winston);
-winston.info("RTS DB API Type: " + rtsDbApi.dbType);
-winston.info("RTS DB API Version: " + rtsDbApi.version);
+let rtsDbApi = require(dbApi)(dbConfig, logger);
+logger.info("RTS DB API Type: " + rtsDbApi.dbType);
+logger.info("RTS DB API Version: " + rtsDbApi.version);
 
 rtsDbApi.init().then(function() {
     // In case the app died with an active meeting.
     rtsDbApi.getActiveMeeting().then(function(mtg) {
         if(mtg !== undefined) {
-            winston.info("Active meeting: " + mtg.meetingId);
+            logger.info("Active meeting: " + mtg.meetingId);
             rtsWsApi.startMeeting(mtg);
             rtsWsApi.refreshWall();
         } else {
-            winston.info("No active meeting.");
+            logger.info("No active meeting.");
         }
     }, function(err) {
-        winston.error("Unable to check for active meeting.");
+        logger.error("Unable to check for active meeting.");
     });
 });
 
-app.use("/api", require("./app/rts-rest-api")(winston, rtsDbApi, rtsWsApi));
+app.use("/api", require("./app/rts-rest-api")(logger, rtsDbApi, rtsWsApi));
 
 // Sacramento County agenda management system access.
 let agendaApi = config.get("SIRE.dbApi");
 let sireConfig = config.get("SIRE.dbConfig");
-let sireApi = require(agendaApi)(sireConfig, winston);
+let sireApi = require(agendaApi)(sireConfig, logger);
 app.get("/sire/meeting", function(req, res) {
-    winston.info("Retrieving meetings from agenda management system.");
+    logger.info("Retrieving meetings from agenda management system.");
     sireApi.getMeetings().then(function(data) {
         res.send(data);
     }, function(err) {
@@ -88,7 +104,7 @@ app.get("/sire/meeting", function(req, res) {
 
 app.get("/sire/item/:meetingId", function(req, res) {
     let meetingId = req.params.meetingId;
-    winston.info("Retrieving items from agenda management system for meetingId: " + meetingId);
+    logger.info("Retrieving items from agenda management system for meetingId: " + meetingId);
     sireApi.getItems(meetingId).then(function(data) {
         res.send(data);
     }, function(err) {
@@ -108,10 +124,10 @@ app.get("/auth/authorize", function(req, res) {
         });
         if(user) {
             res.status(200).send(JSON.stringify(user.groups));
-            winston.info("Authorized access: " + userId);
+            logger.info("Authorized access: " + userId);
         } else {
             res.status(403).send("Forbidden");
-            winston.error("Attemtped unauthorized access: " + userId);
+            logger.error("Attemtped unauthorized access: " + userId);
         }
     }
 });
