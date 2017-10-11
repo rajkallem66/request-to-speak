171fe54396f00a13218c0cdfa0ecdc5b09fce2b0
Guy Sperry <sperrygu@saccounty.net>
2017-10-10 17:47:46 -0700

    Fix SIRE API location issue. Add error handling. Add default log transport.

--- client/admin/main.js
+++ client/admin/main.js
@@ -35,7 +35,7 @@ define(["durandal/system", "durandal/app", "durandal/viewLocator", "bootstrap"],
     });
 
     app.apiLocation = location.href.replace(/[^/]*$/, "") + "api/";
-    app.agendaLocation = location.href.replace(/[^/]*$/, "") + "agenda/";
+    app.agendaLocation = location.href.replace(/[^/]*$/, "") + "sire/";
     
     app.start().then(function() {
         // Replace 'viewmodels' in the moduleId with 'views' to locate the view.
