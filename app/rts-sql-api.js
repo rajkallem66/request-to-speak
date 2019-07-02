5a2ee3e252b4be02195994dedffbf305f14e0400
kallemr <kallemr@saccounty.net>
2019-07-02 15:21:28 -0700

    fix:RTS - 175

--- app/rts-sql-api.js
+++ app/rts-sql-api.js
@@ -353,7 +353,9 @@ function getRequests(meetingId) {
     return new Promise(function(fulfill, reject) {
         let request = pool.request();
             let requestQuery = "SELECT requestId, item.itemOrder, item.itemName, dateAdded, firstName, lastName, " +
-                "case when official = 1 then 'Yes' else 'No' End as official, agency, offAgenda, subTopic, stance, notes, phone, email, address, Request.timeToSpeak, " +
+                "case when official = 1 then 'Yes' else 'No' End as official, agency, offAgenda, subTopic, stance," + 
+                "case when Request.status = 'removed' then 'Yes' else 'No' End as concluded ," +
+                "notes, phone, email, address, Request.timeToSpeak, " +
                 "Request.status, approvedForDisplay FROM Request INNER JOIN Item ON Request.item = Item.itemId " +
                 "INNER JOIN Meeting on Request.meetingId = Meeting.meetingId WHERE Meeting.meetingId = @meetingId " +
                 "ORDER BY Item.itemOrder asc, official desc,  Request.dateAdded asc";
